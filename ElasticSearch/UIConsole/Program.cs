28ad3b431fba653e671ddd1bac10849a6610c9b9
Subham Saraf <ssaraf@tavisca.com>
2017-09-25 10:10:52 +0530

    Data Getting posted and index is being created

---
+++ ElasticSearch/UIConsole/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nest;
+using Elasticsearch.Net;
+
+namespace UIConsole
+{
+    class Program
+    {
+        public static Uri node;
+        public static ConnectionSettings settings;
+        public static ElasticClient client;
+
+
+        static void Main(string[] args)
+        {
+            node = new Uri("http://172.16.14.205:9200");
+            //var connectionPool = new SingleNodeConnectionPool(node);
+            settings=new ConnectionSettings(node).DefaultIndex("lucene_search");            
+            client = new ElasticClient(settings);
+
+            var postDocumenttest = new Postdocument { UserId = 456, PostDate = DateTime.Now, PostText = "Hey this is my first posted Document" };
+            var indexRespose = client.Index(postDocumenttest);
+
+            var searchRespose = client.Search<Postdocument>(s => s.From(0).Size(0)
+            .Query(q => q.Match(m => m.Field(f => f.PostText)
+            .Query("Hey this is my first posted Document"))));
+            var documents = searchRespose.Documents;
+        }
+    }
+}
